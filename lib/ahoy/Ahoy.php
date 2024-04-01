b9ae106f99ad2cd7b9d286d832a597a8670b4143
Seán Stickle <sean@stickle.net>
2024-03-31 21:45:02 -0400

    remove type hints from consts (invalid)

--- lib/ahoy/Ahoy.php
+++ lib/ahoy/Ahoy.php
@@ -4,8 +4,8 @@ namespace Ahoy;
 
 class Ahoy
 {
-    const int VISIT_DURATION = 4 * 60 * 60; // 4 hours
-    const bool REST_API_ONLY = false;
+    const VISIT_DURATION = 4 * 60 * 60; // 4 hours
+    const REST_API_ONLY = false;
 
     public static function log(string $message): void
     {
